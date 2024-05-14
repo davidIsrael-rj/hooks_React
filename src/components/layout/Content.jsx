73b41ed92f56a01fee48c4716748e72703d3cac7
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-14 20:12:39 -0300

    Componente Content e o seu CSS

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,8 +1,15 @@
+import "./Content.css"
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../views/examples/Home";
+import NotFound from "../../views/examples/NotFound";
 
-const Content = props =>(
+const Content = props => (
     <main className="Content">
-        <div>Conteudo</div>
+        <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
     </main>
 )
 
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -4,13 +4,12 @@ import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content';
 import { BrowserRouter } from 'react-router-dom';
 
-
 const App = props => {
     return (
         <div className='App'>
             <BrowserRouter>
                 <Menu />
-                <Content />
+                <Content />                
             </BrowserRouter>
         </div>
     )
