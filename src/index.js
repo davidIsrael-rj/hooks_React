720818e9910d20ce0d67f41354aefbe1556d3fe6
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-13 21:13:02 -0300

    Criando a estrutura do projeto, componente App, pastas components/layout e a pagina Home de exemplo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './index.css';
 import ReactDom from "react-dom";
 import React from 'react';
+import App from './views/App';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<h1>OLa</h1>);
+root.render(
+    <App />
+);
---
+++ src/views/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+
+const App = props => {
+    return(
+        <div className='App'>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default App
