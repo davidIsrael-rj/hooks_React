6bd7a60c2cd69ce7be110882d515da585b67150d
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-19 17:41:55 -0300

    Implementando o useCallback()

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import UseCallbackButtons from "./UseCallbackButtons";
 
-const UseCallback = (props)=>{
-    return(
+
+const UseCallback = (props) => {
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function (delta) {
+        setCount(curr => curr + delta)
+    }, [setCount])
+
+    // function inc(delta) {
+    //     setCount(count + delta)
+    // }
+    return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
+                subtitle="Retorna uma função memorizada!"
             />
+            <div className="center">
+                <spant className="text">{count}</spant>
+                <div>
+                    <UseCallbackButtons inc={inc} />
+                </div>
+            </div>
         </div>
     )
 }
