91161d20a7cf4fc4e809f4e9f78cdaefe51ed39c
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-20 17:58:16 -0300

    Implementando o useReducer – EX#01

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 
-const UseReducer = (props)=>{
-    return(
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    //foco
+    number: 0
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'adicionar2':
+            return { ...state, number: state.number + 2 }
+        case 'login':
+            return { ...state, user: { name: action.payload } }
+        default:
+            return state
+    }
+}
+
+const UseReducer = (props) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const [login, setLogin] = useState("")
+
+    return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+            <div className="center">
+                {state.user ? <span className="text">{state.user.name}</span>
+                    : <span className="text">Sem Usuário</span>
+                }
+                <span className="text">{state.number}</span>
+                <div>
+                    <input type="text" className="input" value={login} onChange={e => setLogin(e.target.value)}/>
+                    <button className="btn" onClick={()=> dispatch({type: 'login', payload: login})}>Login</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'adicionar2' })}>+2</button>
+                </div>
+            </div>
         </div>
     )
 }
