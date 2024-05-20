d5ef6f0aae30ee0afa4e3ece8699eb237d53b8c5
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-20 15:16:20 -0300

    Implementando o useContext – EX#01

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDom from "react-dom/client";
 import React from 'react';
 import App from './views/App';
 
+
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+   
+        <App />
+   
 );
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,17 +1,21 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content';
 import { BrowserRouter } from 'react-router-dom';
+import DataContext, { data } from '../data/DataContext';
 
 const App = props => {
+    const [state, setState] = useState(data)
     return (
-        <div className='App'>
-            <BrowserRouter>
-                <Menu />
-                <Content />                
-            </BrowserRouter>
-        </div>
+        <DataContext.Provider value={{state, setState}}>
+            <div className='App'>
+                <BrowserRouter>
+                    <Menu />
+                    <Content />
+                </BrowserRouter>
+            </div>
+        </DataContext.Provider>
     )
 }
 
