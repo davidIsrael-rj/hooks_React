ffea303d1c0b2af93ccc52b40451e433b032eef5
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-23 03:45:19 -0300

    Implementando Custon Hooks  - Exercício #02

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,11 +2,21 @@ import React, { useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 import { useCounter } from "../../hooks/useCounter";
+import { useFetch } from "../../hooks/useFetch";
 
 const UseCustom = (props) => {
 
     const [count, inc, dec, atualizar] = useCounter()
     const [n, setN] = useState(0)
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const response = useFetch(url)
+
+    function showStates(states) {
+        return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla}</li>)
+    }
+    // http://files.cod3r.com.br/curso-react/estados.json
+
+
     return (
 
         <div className="UseCustom">
@@ -17,12 +27,18 @@ const UseCustom = (props) => {
             <SectionTitle title={"Exercício #01"} />
             <div className="center">
                 <span className="text">{count}</span>
-                <input type="number" className="input" value={n} onChange={e => setN(e.target.value)}/>
+                <input type="number" className="input" value={n} onChange={e => setN(e.target.value)} />
                 <div>
-                    <button className="btn" onClick={()=>inc()}>+</button>
-                    <button className="btn" onClick={()=>dec()}>-</button>
-                    <button className="btn"onClick={()=>atualizar(+n)}>Atualizar</button>
+                    <button className="btn" onClick={() => inc()}>+</button>
+                    <button className="btn" onClick={() => dec()}>-</button>
+                    <button className="btn" onClick={() => atualizar(+n)}>Atualizar</button>
                 </div>
+            </div>
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <ul>
+                    {response.data ? showStates(response.data): false}
+                </ul>
             </div>
         </div>
     )
